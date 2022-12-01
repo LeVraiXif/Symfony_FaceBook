812beee1125e1106033d19e18751b72f6fe9437a
Xif <tmxif732@gmail.com>
2022-12-01 16:32:36 +0100

    Api + Token

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -2,22 +2,34 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\PostRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: PostRepository::class)]
+#[ApiResource(
+    collectionOperations: ['get' => ['normalization_context' => ['groups' => 'Post:list']], 'post' => ['normalization_context' => ['groups' => 'Post:list']]],
+    itemOperations: ['get' => ['normalization_context' => ['groups' => 'Post:item']], 'put' => ['normalization_context' => ['groups' => 'Post:item']], 'patch' => ['normalization_context' => ['groups' => 'Post:item']], 'delete' => ['normalization_context' => ['groups' => 'Post:item']]],
+    order: ['message' => 'DESC', 'id' => 'ASC'],
+    paginationEnabled: false,
+)]
 class Post
+
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['Post:list', 'Post:item'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 2000)]
+    #[Groups(['Post:list', 'Post:item'])]
     private ?string $message = null;
 
     #[ORM\ManyToOne(inversedBy: 'posts')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['Post:list', 'Post:item'])]
     private ?User $user = null;
 
     public function getId(): ?int
